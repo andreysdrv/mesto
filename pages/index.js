64f4c7ac4e99e50eb0e783c436d616f5c5c5c97c
Andrew <totalretard4@gmail.com>
2021-02-19 11:47:49 +0300

    fix: дополнить код поп-апа

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,31 @@
 let popupOpen = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupClose = document.querySelector('.popup__close-button');
+let userName = document.querySelector('.popup__input-name');
+let userAbout = document.querySelector('.popup__input-about');
+let popupButton = document.querySelector('.popup__button');
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
 
-console.log(popupOpen);
-console.log(popup);
-console.log(popupClose);
+function popUP () {
+  if (popup.classList.contains('popup_opened') === false) {
+    popupOpen.addEventListener('click', function () {
+      popup.classList.toggle('popup_opened');
+      userName.value = '';
+      userAbout.value = '';
+    });
+  } 
+    popupClose.addEventListener('click', function () {
+    popup.classList.toggle('popup_opened');
+  });
+    popupButton.addEventListener('click', function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent = userName.value;
+    profileAbout.textContent = userAbout.value;
+  });
+  popupButton.addEventListener('click', function () {
+    popup.classList.toggle('popup_opened');
+  });
+}
 
-function open () {
-  popup.classList.add('popup_opened');
-};
-
-popupOpen.addEventListener('click', open);
-
-function close () {
-  popup.classList.remove('popup_opened');
-};
-
-popupClose.addEventListener('click', close);
+popUP ();
