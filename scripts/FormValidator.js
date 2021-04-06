ccb68b42a55d04da1007767c834bb398adda5d90
Andrew <totalretard4@gmail.com>
2021-04-06 12:03:40 +0300

    fix: перенести поиск инпутов и кнопки сабмита в конструктор класса FromValidator

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -7,6 +7,10 @@ export class FormValidator {
     this._errorClass = data.errorClass
     
     this._formElement = formElement
+
+    this._inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector))
+    this._buttonElement = this._formElement.querySelector(this._submitButtonSelector)
+
   }
 
   _showError(inputElement, errorMessage) {
@@ -36,30 +40,27 @@ export class FormValidator {
     })
   }
 
-  _toggleButtonState(inputList, buttonElement) {
-    if (this._hasInvalidInput(inputList)) {
-      buttonElement.classList.add(this._inactiveButtonClass)
-      buttonElement.disabled = true
+  _toggleButtonState() {
+    if (this._hasInvalidInput(this._inputList)) {
+      this._buttonElement.classList.add(this._inactiveButtonClass)
+      this._buttonElement.disabled = true
     } else {
-      buttonElement.classList.remove(this._inactiveButtonClass)
-      buttonElement.disabled = false
+      this._buttonElement.classList.remove(this._inactiveButtonClass)
+      this._buttonElement.disabled = false
     }
   }
 
   _setIventListeners() {
-    const inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector))
-    const buttonElement = this._formElement.querySelector(this._submitButtonSelector)
-    this._toggleButtonState(inputList, buttonElement)
-    inputList.forEach((inputElement) => {
+    this._toggleButtonState()
+    this._inputList.forEach((inputElement) => {
       inputElement.addEventListener('input', () => {
         this._checkInputValidity(inputElement)
-        this._toggleButtonState(inputList, buttonElement)
+        this._toggleButtonState()
       })
     })
   }
 
   disableSubmitButton() {
-    this._buttonElement = this._formElement.querySelector(this._submitButtonSelector)
     this._buttonElement.classList.add(this._inactiveButtonClass);
     this._buttonElement.disbaled = true;
   }
